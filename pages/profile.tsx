d784c185c9909d2ab29e8ea125000cdaeac8ae07
Victor Alessander <alessander@protonmail.com>
2023-03-29 14:14:08 -0300

    fix: reusing head tag
    fix: missing head tag for profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,3 +1,4 @@
+import Meta from "@partials/meta";
 import { Divider, Button, Form, Input } from "antd";
 
 export default function Profile() {
@@ -5,38 +6,41 @@ export default function Profile() {
   const [formTwo] = Form.useForm();
 
   return (
-    <main>
-      <Divider orientation="left">Registration Data</Divider>
+    <>
+      <Meta />
+      <main>
+        <Divider orientation="left">Registration Data</Divider>
 
-      <Form layout="vertical" form={formOne} style={{ maxWidth: 600 }}>
-        <Form.Item label="Email">
-          <Input
-            type="text"
-            placeholder="Enter an email"
-            value="anyemail@townsend.com"
-          />
-        </Form.Item>
-        <Form.Item label="Name">
-          <Input type="text" placeholder="Enter a name" value="any name" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary">Submit</Button>
-        </Form.Item>
-      </Form>
+        <Form layout="vertical" form={formOne} style={{ maxWidth: 600 }}>
+          <Form.Item label="Email">
+            <Input
+              type="text"
+              placeholder="Enter an email"
+              value="anyemail@townsend.com"
+            />
+          </Form.Item>
+          <Form.Item label="Name">
+            <Input type="text" placeholder="Enter a name" value="any name" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary">Submit</Button>
+          </Form.Item>
+        </Form>
 
-      <Divider orientation="left">Change Password</Divider>
+        <Divider orientation="left">Change Password</Divider>
 
-      <Form form={formTwo} layout="vertical" style={{ maxWidth: 600 }}>
-        <Form.Item>
-          <Input type="password" placeholder="Current Password" />
-        </Form.Item>
-        <Form.Item>
-          <Input type="password" placeholder="New Password" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary">Submit</Button>
-        </Form.Item>
-      </Form>
-    </main>
+        <Form form={formTwo} layout="vertical" style={{ maxWidth: 600 }}>
+          <Form.Item>
+            <Input type="password" placeholder="Current Password" />
+          </Form.Item>
+          <Form.Item>
+            <Input type="password" placeholder="New Password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary">Submit</Button>
+          </Form.Item>
+        </Form>
+      </main>
+    </>
   );
 }
